88829f038d723f46105ae877cf34806081e839ac
Tim Benniks <tbenniks@gmail.com>
2022-09-24 11:01:01 +0200

    back to basics for debug, step 12

--- pages/[[slug]].vue
+++ pages/[[slug]].vue
@@ -12,21 +12,21 @@ if (!data.value) {
 }
 
 const { data: composition } = await useEnhance(data);
-// const title = composition.value.parameters?.title?.value || "No Title";
-// const description =
-//   composition.value.parameters?.description?.value || "No Description";
-// const image = composition.value.parameters.image;
+const title = composition.value.parameters?.title?.value || "No Title";
+const description =
+  composition.value.parameters?.description?.value || "No Description";
+const image = composition.value?.parameters?.image;
 
-// usePageMeta({
-//   title: title as string,
-//   description: description as string,
-//   slug: route.params.slug as string,
-//   image:
-//     (image?.value[0]?.baseurl.replace(
-//       "image/upload/",
-//       "image/upload/w_1200,"
-//     ) as string) || "",
-// });
+usePageMeta({
+  title: title as string,
+  description: description as string,
+  slug: route.params.slug as string,
+  image:
+    (image?.value[0]?.baseurl.replace(
+      "image/upload/",
+      "image/upload/w_1200,"
+    ) as string) || "",
+});
 </script>
 <template>
   <main class="max-w-[1440px] mx-auto pt-36">
