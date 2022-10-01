cc48b4ac98768aa9148d47f8c5518b42fc087d2a
Tim Benniks <tbenniks@gmail.com>
2022-10-01 22:04:43 +0200

    better graphs

--- components/PersonalizationStats.client.vue
+++ components/PersonalizationStats.client.vue
@@ -1,51 +1,109 @@
 <script lang="ts" setup>
-useHead({
-  script: [
-    {
-      hid: "donutty",
-      src: "https://cdn.jsdelivr.net/npm/donutty@2.3.1/dist/donutty.min.js",
-      defer: true,
-    },
-  ],
-});
+function hashCode(str: string) {
+  let hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
 
-const scores = ref(usePersonalizationScores());
+function pickColor(str: string) {
+  return `hsl(${hashCode(str) % 360}, 100%, 50%)`;
+}
+
+function parseScores() {
+  const scores = usePersonalizationScores();
+
+  if (scores.length === 0) {
+    return {
+      interests: false,
+      complexity: false,
+    };
+  }
+
+  const interestValues = scores.filter((c) => c.category === "Interest");
+  const complexityValues = scores.filter((c) => c.category === "Complexity");
+
+  const interests = {
+    cap: interestValues[0].cap,
+    label: interestValues[0].category,
+    values: interestValues,
+  };
+
+  const complexity = {
+    cap: complexityValues[0].cap,
+    label: complexityValues[0].category,
+    values: complexityValues,
+  };
+
+  return {
+    interests,
+    complexity,
+  };
+}
+
 const { $useUniformContext } = useNuxtApp();
 const { context } = $useUniformContext();
+const scores = ref(parseScores());
 
 async function forgetMe() {
   await context.forget(true);
-  scores.value = usePersonalizationScores();
+  scores.value = parseScores();
+}
+
+const interestDonut = ref({});
+const complexityDonut = ref({});
+
+if (scores.value.interests) {
+  interestDonut.value = {
+    size: 160,
+    sections: scores.value.interests.values.map((val) => {
+      return {
+        label: val.value,
+        value: val.score,
+        color: pickColor(val.value),
+      };
+    }),
+    thickness: 40,
+    hasLegend: true,
+    foreground: "rgba(37, 106, 209, 1)",
+    background: "rgba(24, 51, 87, 1)",
+    total: scores.value.interests?.cap || 100,
+    legendPlacement: "bottom",
+  };
+}
+
+if (scores.value.complexity) {
+  complexityDonut.value = {
+    size: 160,
+    sections: scores.value.complexity.values.map((val) => {
+      return {
+        label: val.value,
+        value: val.score,
+        color: pickColor(val.value),
+      };
+    }),
+    thickness: 40,
+    hasLegend: true,
+    foreground: "rgba(37, 106, 209, 1)",
+    background: "rgba(24, 51, 87, 1)",
+    total: scores.value.complexity?.cap || 100,
+    legendPlacement: "bottom",
+  };
 }
 </script>
 
 <template>
-  <div v-if="scores.length > 0">
-    <ul class="grid grid-cols-3 gap-8">
-      <li v-for="score in scores" :key="score.value" class="relative block">
-        <p class="text-center font-bold text-xl mb-2">{{ score.category }}</p>
-        <div
-          class="w-32 h-32 aspect-[32/32] mx-auto overflow-hidden"
-          data-donutty
-          data-min="0"
-          data-thickness="25"
-          data-round="true"
-          data-circle="false"
-          :data-max="score.cap"
-          :data-value="score.score"
-          data-color="#D1258C"
-          data-padding="10"
-          data-bg="rgba(37, 106, 209, 0.40)"
-        ></div>
-        <p class="relative text-center -top-[5rem] font-bold text-xl">
-          {{ score.score }}%
-        </p>
-        <p class="text-center relative -top-[2rem]">{{ score.value }}</p>
-      </li>
-    </ul>
+  <div v-if="scores.interests || scores.complexity">
+    <div class="grid grid-cols-2 gap-8 mb-4">
+      <vc-donut v-bind="interestDonut">{{ scores.interests.label }}</vc-donut>
+      <vc-donut v-bind="complexityDonut">{{
+        scores.complexity.label
+      }}</vc-donut>
+    </div>
     <p>
       Not entirly happy with your profile? Click
-      <button @click="forgetMe">Forget me</button> to start over.
+      <button @click="forgetMe">forget me</button> to start over.
     </p>
   </div>
 
